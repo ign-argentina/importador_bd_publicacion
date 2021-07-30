43cac850e06445eb0935f3c8af34531b982b2e00
andreazomoza <aszomoza@gmail.com>
2021-07-30 15:30:15 -0300

    actualizacion codigo para nueva version php

--- LibStrings.php
+++ LibStrings.php
@@ -10,12 +10,12 @@ https://zetawiki.com/wiki/%EC%A0%9C%ED%83%80%EC%9C%84%ED%82%A4_zetawiki
 function utf8_ord($ch) {
   $len = strlen($ch);
   if($len <= 0) return false;
-  $h = ord($ch{0});
+  $h = ord($ch[0]);
   if ($h <= 0x7F) return $h;
   if ($h < 0xC2) return false;
-  if ($h <= 0xDF && $len>1) return ($h & 0x1F) <<  6 | (ord($ch{1}) & 0x3F);
-  if ($h <= 0xEF && $len>2) return ($h & 0x0F) << 12 | (ord($ch{1}) & 0x3F) << 6 | (ord($ch{2}) & 0x3F);          
-  if ($h <= 0xF4 && $len>3) return ($h & 0x0F) << 18 | (ord($ch{1}) & 0x3F) << 12 | (ord($ch{2}) & 0x3F) << 6 | (ord($ch{3}) & 0x3F);
+  if ($h <= 0xDF && $len>1) return ($h & 0x1F) <<  6 | (ord($ch[1]) & 0x3F);
+  if ($h <= 0xEF && $len>2) return ($h & 0x0F) << 12 | (ord($ch[1]) & 0x3F) << 6 | (ord($ch[2]) & 0x3F);          
+  if ($h <= 0xF4 && $len>3) return ($h & 0x0F) << 18 | (ord($ch[1]) & 0x3F) << 12 | (ord($ch[2]) & 0x3F) << 6 | (ord($ch[3]) & 0x3F);
   return false;
 }
 
